37cf00b3fa05b61d6e72d51574d6c3dd87f053a2
Aaron Cox <aaron@greymass.com>
2022-05-10 12:37:29 -0400

    Added Windows build
    Related #1

---
+++ build/windows/electron-builder.js
@@ -0,0 +1,32 @@
+const common = require('../common/electron-builder.js')
+
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+    win: {
+        icon: 'build/assets/icons/win/icon.ico',
+        publisherName: 'Greymass Inc.',
+        target: [
+            {
+                target: 'nsis',
+                arch: ['x64', 'ia32'],
+            },
+        ],
+    },
+    nsis: {
+        allowToChangeInstallationDirectory: true,
+        createDesktopShortcut: 'always',
+        createStartMenuShortcut: true,
+        deleteAppDataOnUninstall: true,
+        oneClick: false,
+        perMachine: true,
+        shortcutName: 'Anchor Wallet',
+    },
+}
+
+module.exports = {
+    ...common,
+    ...config,
+}
