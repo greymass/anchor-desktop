671b429a6fb7a51771e772365e7ef840a051233e
Aaron Cox <aaron@greymass.com>
2022-05-18 14:18:17 -0400

    Simplified messaging for protocol handlers

--- packages/core/src/modules/handler.ts
+++ packages/core/src/modules/handler.ts
@@ -4,20 +4,19 @@ import {log as logger} from '~/modules/log'
 const log = logger.scope('handler')
 
 export const enableHandler = () => {
-    log.info('modules/handler: enable')
     // Setup generic esr:// handlers
     app.setAsDefaultProtocolClient('esr')
     protocol.registerHttpProtocol('esr', (req, cb) => {
-        log.info('modules/handler: register', req, cb)
+        log.info('Registered esr: protocol', req, cb)
     })
     // Setup Anchor specific anchor:// handlers
     app.setAsDefaultProtocolClient('anchor')
     protocol.registerHttpProtocol('anchor', (req, cb) => {
-        log.info('modules/handler: register', req, cb)
+        log.info('Registered anchor: protocol', req, cb)
     })
     // Setup account creation via Anchor specific anchorcreate:// handlers
     app.setAsDefaultProtocolClient('anchorcreate')
     protocol.registerHttpProtocol('anchorcreate', (req, cb) => {
-        log.info('modules/handler: register', req, cb)
+        log.info('Registered anchorcreate: protocol', req, cb)
     })
 }
