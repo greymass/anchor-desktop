f1836a5045e322827765a6797c05bbd0432641d1
Aaron Cox <aaron@greymass.com>
2022-08-22 17:35:16 -0400

    Move URI handlers to array, added esr-anchor handler.

--- packages/core/src/modules/handler.ts
+++ packages/core/src/modules/handler.ts
@@ -3,20 +3,13 @@ import {log as logger} from '~/modules/log'
 
 const log = logger.scope('handler')
 
+const uris = ['esr', 'esr-anchor', 'anchor', 'anchorcreate']
+
 export const enableHandler = () => {
-    // Setup generic esr:// handlers
-    app.setAsDefaultProtocolClient('esr')
-    protocol.registerHttpProtocol('esr', (req, cb) => {
-        log.info('Registered esr: protocol', req, cb)
-    })
-    // Setup Anchor specific anchor:// handlers
-    app.setAsDefaultProtocolClient('anchor')
-    protocol.registerHttpProtocol('anchor', (req, cb) => {
-        log.info('Registered anchor: protocol', req, cb)
-    })
-    // Setup account creation via Anchor specific anchorcreate:// handlers
-    app.setAsDefaultProtocolClient('anchorcreate')
-    protocol.registerHttpProtocol('anchorcreate', (req, cb) => {
-        log.info('Registered anchorcreate: protocol', req, cb)
+    uris.forEach((uri) => {
+        app.setAsDefaultProtocolClient(uri)
+        protocol.registerHttpProtocol(uri, (req, cb) => {
+            log.info('Registered esr: protocol', req, cb)
+        })
     })
 }
