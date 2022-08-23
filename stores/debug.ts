e524ffc56b8a983bde35f55d02026505c1472173
Aaron Cox <aaron@greymass.com>
2022-08-23 16:44:45 -0400

    Adding debug store with mock data for testing purposes.

---
+++ stores/debug.ts
@@ -0,0 +1,17 @@
+import {PermissionLevel, PrivateKey} from '@greymass/eosio'
+import {Writable} from 'svelte/store'
+import {setupWritable} from '@stores'
+
+export const currentChainId = '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840'
+
+export const activeAuthority: Writable<PermissionLevel> = setupWritable(
+    'activeAuthority',
+    PermissionLevel.from({
+        actor: 'corecorecore',
+        permission: 'active',
+    })
+)
+
+export const privateKey: PrivateKey = PrivateKey.from(
+    '5JW71y3njNNVf9fiGaufq8Up5XiGk68jZ5tYhKpy69yyU9cr7n9'
+)
