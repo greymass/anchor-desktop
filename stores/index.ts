d39c363c095c0003ff847d22cd69a0c79517e17d
Aaron Cox <aaron@greymass.com>
2022-08-23 16:44:26 -0400

    Renamed and duplicated setup methods for different types

--- stores/index.ts
+++ stores/index.ts
@@ -1,4 +1,10 @@
-import {writable} from 'svelte/store'
+import {
+    derived as svelteDerived,
+    Readable,
+    readable as svelteReadable,
+    Unsubscriber,
+    writable as svelteWritable,
+} from 'svelte/store'
 import type {Writable} from 'svelte/store'
 import {PrimaryStore, ReplicatedStore} from 'svelte-channel-store'
 import {Name, Serializer} from '@greymass/eosio'
@@ -11,10 +17,10 @@ const options = {
     decode: (value) => (isRaw(value) ? toCore(value) : value),
 }
 
-function setupStore<T>(name: string, initialValue?: T): Writable<T> {
+export function setupReadable<T>(name: string, initialValue?: T): Readable<T> {
     // TODO: we don't need to setup a new channel for each store. do a check and setup only once
     if (isMain) {
-        const store = new PrimaryStore<T>(name, writable(initialValue), options)
+        const store = new PrimaryStore<T>(name, svelteReadable(initialValue), options)
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         require('electron').ipcMain.on('store', (event, storeName) => {
             if (storeName !== name) {
@@ -29,7 +35,61 @@ function setupStore<T>(name: string, initialValue?: T): Writable<T> {
         return store
     } else {
         const channel = new MessageChannel()
-        const store = new ReplicatedStore<T>(name, writable(initialValue), options)
+        const store = new ReplicatedStore<T>(name, svelteReadable(initialValue), options)
+        store.attach(channel.port2)
+        window.postMessage({type: 'store', port: channel.port1, name}, '*', [channel.port1])
+        return store
+    }
+}
+
+export function setupWritable<T>(name: string, initialValue?: T): Writable<T> {
+    // TODO: we don't need to setup a new channel for each store. do a check and setup only once
+    if (isMain) {
+        const store = new PrimaryStore<T>(name, svelteWritable(initialValue), options)
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require('electron').ipcMain.on('store', (event, storeName) => {
+            if (storeName !== name) {
+                return
+            }
+            const port = event.ports[0] as any
+            // fix electrons broken port implementation
+            port.addEventListener = (name: any, fn: any) => port.addListener(name, fn)
+            port.removeEventListener = (name: any, fn: any) => port.removeListener(name, fn)
+            store.attach(port)
+        })
+        return store
+    } else {
+        const channel = new MessageChannel()
+        const store = new ReplicatedStore<T>(name, svelteWritable(initialValue), options)
+        store.attach(channel.port2)
+        window.postMessage({type: 'store', port: channel.port1, name}, '*', [channel.port1])
+        return store
+    }
+}
+
+export function setupDerived<T>(
+    name: string,
+    stores: any,
+    fn: (values: unknown, set: (value: T) => void) => void | Unsubscriber
+): Writable<T> {
+    // TODO: we don't need to setup a new channel for each store. do a check and setup only once
+    if (isMain) {
+        const store = new PrimaryStore<T>(name, svelteDerived(stores, fn), options)
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require('electron').ipcMain.on('store', (event, storeName) => {
+            if (storeName !== name) {
+                return
+            }
+            const port = event.ports[0] as any
+            // fix electrons broken port implementation
+            port.addEventListener = (name: any, fn: any) => port.addListener(name, fn)
+            port.removeEventListener = (name: any, fn: any) => port.removeListener(name, fn)
+            store.attach(port)
+        })
+        return store
+    } else {
+        const channel = new MessageChannel()
+        const store = new ReplicatedStore<T>(name, svelteDerived(stores, fn), options)
         store.attach(channel.port2)
         window.postMessage({type: 'store', port: channel.port1, name}, '*', [channel.port1])
         return store
@@ -45,5 +105,5 @@ const toRaw = (data: any) => ({
     type: data.constructor.abiName,
 })
 
-export const sharedName = setupStore('sharedName', Name.from('foo'))
-export const sharedInfo = setupStore('sharedInfo')
+export const sharedName = setupWritable('sharedName', Name.from('foo'))
+export const sharedInfo = setupWritable('sharedInfo')
