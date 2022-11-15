08e26197e6c17e7268b1596f05e4f6f6b3e5b97e
dafuga <danielfugere28@gmail.com>
2022-11-14 19:02:04 -0500

    chore: added ricardian contract generation to project

---
+++ src/packages/signer/src/components/RicardianContract.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+    let abi
+    let transaction
+
+    const ricardianScript =  `
+      var contractContainer = document.getElementById("contract-container");
+
+      var factory = new ContractTemplateToolkit.RicardianContractFactory();
+      var config = {
+        abi: ${abi},
+        transaction: ${transaction},
+        actionIndex: 0,
+        maxPasses: 3,
+        allowUnusedVariables: false,
+      };
+      var ricardianContract = factory.create(config);
+      var html = ricardianContract.getHtml();
+
+      contractContainer.innerHTML = html;
+
+      window.ReactNativeWebView.postMessage(document.body.scrollHeight);
+
+      true; // note: this is required, or you'll sometimes get silent failures
+    `
+</script>
+
+<div>
+    <iframe src={ricardianPath} />
+</div>
